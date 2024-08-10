2beaa6f6fa8cd45e89c99d0ae498d271d6934414
ayushkrl <111305534+ayushkrl@users.noreply.github.com>
2024-08-10 11:50:20 +0530

    first commit

---
+++ client/src/post.js
@@ -0,0 +1,20 @@
+import {format} from 'date-fns';
+
+export default function Post({title, summary, cover, content, createdAt,author})  {
+    return (
+        <div className='post'>
+        <div className='image'>
+          <img src={'http://localhost:4000/'+cover} alt=""/>
+        </div>
+
+        <div className='texts'>
+          <h2>{title}</h2>
+          <p className='info'>
+            <a className='author'>{author.username}</a>
+            <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
+          </p>
+          <p className='summary'>{summary}</p>
+        </div>
+      </div>
+    )
+}
